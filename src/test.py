d955e9fd6f526d915afb9d2e895b98560ab4b546
Adithya Kumar <111501017@smail.iitpkd.ac.in>
2018-03-06 20:10:00 +0530

    In test, added constraing as a boolean function, can use these to loop ans store

--- src/test.py
+++ src/test.py
@@ -26,9 +26,104 @@ duration[t, s, g, n] = 2
 duration = np.ones([num_t, num_s, num_g, n_max])
 
 '''
-node can be representated as ('x', t, s, g, n)
+node can be representated as ('x', t, s, g, n, d, p)
 graph will be the same but these tuples as nodes
 '''
+
+def bic1(t, s, g, n, d, p1, p2):
+	truth = [
+		(p1 > np.min(periods[d])),
+		(p1 <= np.max(periods[d]) - duration[t, s, g, n] + 1),
+		(p2 >= p1),
+		(p2 <= p1 + duration[t, s, g, n] - 1)
+	]
+	return np.prod(truth)
+
+
+def bic2(t, s, g, n, d, p1, p2):
+	truth = [
+		(p1 <= p2),
+		(p1 >= p2 - duration[t, s, g, n] + 1),
+		(p1 >= np.min(periods[d])),
+		(p1 <= np.max(periods[d]) - duration[t, s, g, n] + 1)
+	]
+	return np.prod(truth)
+
+def bic3(d, p):
+	truth = [
+		(p <= np.max(periods[d])),
+		(p >= np.min(periods[d]))
+	]
+	return np.prod(truth)
+
+def bic4(d, p):
+	truth = [
+		(p in periods[d])
+	]
+	return np.prod(truth)
+
+def bic5(t, s, g, n, d, p):
+	truth = [
+		(p1 >= np.min(periods[d])),
+		(p1 <= np.max(periods[d]) - duration[t, s, g, n] + 1)
+	]
+	return np.prod(truth)
+
+def bic6(t, s, g, n):
+	# this means tsgn belongs to lessons[t] and to lessons[g]
+	truth = [
+		(duration[t, s, g, n] > 0)
+	]
+	return np.prod(truth)
+
+
+def bic7(d):
+	truth = [
+		(d in days)
+	]
+	return np.prod(truth)
+
+
+def bic8(k, d):
+	truth = [
+		(k >= 1),
+		(k <= len(periods[d]) - 2),
+		(p >= np.min(periods[d]) + 1),
+		(p <= np.max(periods[d]) - k)
+	]
+	return np.prod(truth)
+
+
+def bic9(k, d):
+	truth = [
+		(k >= 1),
+		(k <= len(periods[d]) - 2)
+	]
+	return np.prod(truth)
+
+def bic10(k, d):
+	truth = [
+		(p >= np.min(periods[d]) + 1),
+		(p <= np.max(periods[d]) - k)
+	]
+	return np.prod(truth)
+
+def bic11(d, p):
+	truth = [
+		(p >= np.min(periods[d]) + 1),
+		(p <= np.max(periods[d]) - 1)
+	]
+	return np.prod(truth)
+
+def bic11(k, d, p):
+	truth = [
+		(k >= 1),
+		(k <= np.max(periods[d]) - p)
+	]
+	return np.prod(truth)
+
+
+
 
 for t in teachers:
 	for s in subjects:
