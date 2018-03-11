e328d3317690a636adc452f6f28e4124fe3ab4fb
Your Name <you@example.com>
2018-03-11 14:50:08 +0530

    created moudles folder

---
+++ src/module/graph2sat/graph.py
@@ -0,0 +1,20 @@
+graph = {}
+
+var01 = 'xtsgndp'
+val11 = (1,2,3,4,5,6)
+val12 = (1,2,4,4,5,6)
+
+var02 = 'xtsgnd'
+val21 = (1,2,3,4,5)
+val22 = (1,2,4,4,5)
+
+graph = {
+	var01 : {
+		val11 : [(var02,val21),['or',(var01,val11),(var01,val12)]],
+		val12 : [(var02,val11)]
+	},
+	var02 : {
+		val21 : [(var01,val11),['an',(var02,val21),(var01,val12)]],
+		val22 : [(var01,val12)]
+	}
+}
---
+++ src/module/graph2sat/parser.py
@@ -0,0 +1,6 @@
+from graph import *
+
+cvars = [[(i,j) for j in (graph[i])] for i in graph]
+
+print cvars
+
