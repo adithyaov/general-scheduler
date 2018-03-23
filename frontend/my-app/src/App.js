686feaf3eefd1e88c0a78c0acd32db934f1b16a7
Sooraj Tom <soorajkandathil@gmail.com>
2018-03-23 23:21:47 +0530

    dynamic constraint rows

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -6,46 +6,52 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      no_s : 3,
-      subs: [{id : 0}, {id : 1}, {id : 2}]
+      no_s : 0,
+      subs: [],
+      no_t : 1,
+      no_g : 1,
     }
   }
-  handleRemoveConstraint = (idx) => {
-    // this.setState({subs: this.state.subs.filter((s, sidx) => idx !== sidx)});
-    console.log("Rem" + idx)
+
+  handleAddConstraint = () => {
+    if(this.state.no_s < 100){
+      this.setState({subs : this.state.subs.concat([{id : this.state.no_s}])});
+      this.setState({no_s : this.state.no_s + 1,})
+    }
   }
-  handleAddConstraint = (idx) => {
-    this.setState({subs : this.state.subs.concat([{id : idx}])});
+
+  handleRemoveConstraint = (idx) => () => {
+    this.setState({subs: this.state.subs.filter((s, sidx) => idx !== sidx)});
+    // console.log("Rem" + idx)
+  }
+  handleChangeNo_t = (evt) => {
+    this.setState({no_t :evt.target.value});
+  }
+  handleChangeNo_g = (evt) => {
+    this.setState({no_g :evt.target.value});
   }
 
   render() {
     return (
       <div className="App">
-        {
-        React.createElement('div', {}, [
-          React.createElement('label', {}, "T: "),
-          React.createElement('input', {type : "number", min : 1}),
-
-          // React.createElement('label', {id: 'no_s', }, "S: "),
-          // React.createElement('input', {type : "number", min : 1}),
-
-          React.createElement('label', {}, "G: "),
-          React.createElement('input', {type : "number", min : 1}),         
-          ]
-          )
-        }
+        <div>
+          <label>T: </label>
+          <input type='number' onChange={this.handleChangeNo_t}/>
+          <label>G: </label>
+          <input type='number' onChange={this.handleChangeNo_g}/>
+        </div>
         <ul>
         {this.state.subs.map((subs, idx) => (
           <li key={subs.id}>
           <div className='constraintBar'>
-            <input type='text' placeholder={`Constraint #${idx + 1}`} />
+            <input type='text' placeholder={`Constraint #${subs.id + 1}`} />
             <button onClick={this.handleRemoveConstraint(idx)} > Remove </button>
           </div>
           </li>
           )
           )}
         </ul>
-        <button onClick={this.handleAddConstraint(this.state.no_s)} > Add constraint </button>
+        <button onClick={this.handleAddConstraint} > Add constraint </button>
         </div>
     );
   }
