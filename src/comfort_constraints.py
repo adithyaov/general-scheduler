99ef6c77ae854373c991ab46e8345abdc1adf511
Jude <judekanil@gmail.com>
2018-03-28 21:46:50 +0530

    blah

--- src/comfort_constraints.py
+++ src/comfort_constraints.py
@@ -1,3 +1,14 @@
+comfort_graph = {}
+
+comfort_graph['xtdp'] = {}
+comfort_graph['xtd'] = {}
+comfort_graph['xtp'] = {}
+comfort_graph['xgdp'] = {}
+comfort_graph['xgd'] = {}
+comfort_graph['xgp'] = {}
+comfort_graph['lkgd'] = {}
+
+
 # 1) Forbidden and requested working hours
 
 teacher_forbidden0 = [(t, d, p)]
@@ -71,7 +82,7 @@ teaching_days = [(t, n)]        # n <= d
 no_of_days = len(days)
 
 for (t, n) in teaching_days:
-    atmost_var_list = [] 
+    atmost_var_list = []
     atleast_var_list = []
     for d in days:
         atmost_var_list.append(('xtd', (t, d)))
@@ -83,24 +94,38 @@ for (t, n) in teaching_days:
     atleast_n = Cardinality(atleast_var_list, (no_of_days - n))
     comfort_true_list.append(atleast_n.form())
 
-for (t, n) in teaching_days:
-    var_list = [] 
-    for d in days:
-        var_list.append(('xtd', (t, d)))
-    
-    atleast_n = Cardinality(var_list, n)
-    comfort_true_list.append(atleast_n.form())
-
-Cardinality(negation('xtd')| d in days) <= |days| - n
-
-
-
-
 # 4) Work day duration: ('xgdp' ∧ 'xgd(p+k−1)') => 'lkgd' min(periods(d)) <= p <= max(periods(d)) - k + 1
 #                     ('lkgd' => (or, ('xgdp' ^ 'xgd(p+k-1)')) min(periods(d)) <= p  <= max(periods(d)) - k+1
 
 #                     also for every working day: 'xgd' => 'lkgd'
 #                     atmost n hours:  negation('lkgd') for all k > n
+
+work_day_duration = [(g, k, d)]
+
+for (g, k, d) in work_day_duration:
+    comfort_graph['lkgd'][(g, k, d)] = []
+
+    or_list = []
+    for p in range(min(periods[d]), max(periods[d]) - k + 2):
+        or_list.append(('and', [('xgdp', (g, d, p)), ('xgdp', (g, d, p + k - 1))]))
+        comfort_true_list.append(('or', [negation(('xgdp', (g, d, p))), negation(('xgdp', (g, d, p + k - 1))), ('lkgd', (k, g, d))]))
+
+    comfort_graph['lkgd'][(g, k, d)].append(('or', or_list))
+
+# Atmost n hours on day d for group g 
+duration_upper_limit = [(g, d, n)]
+
+for (g, d, n) in duration_upper_limit:
+    for k in range(n, max(periods[d]) + 1):
+        comfort_true_list.append(negation(('lkgd', (k, g, d))))
+
+# Atleast n hours on day d for group g
+duration_lower_limit = [(g, d, n)]
+
+for (g, d, n) in duration_lower_limit:
+    for 
+
+
 
 # 5) Idle duration cardinality('xtgdp')
 
