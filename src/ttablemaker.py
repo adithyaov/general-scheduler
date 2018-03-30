dbea685725e963e566393101fbfb30da831e4fcd
immkg <131501018@smail.iitpkd.ac.in>
2018-03-30 18:59:14 +0530

    Created course IDs and account for multiple sol

--- src/ttablemaker.py
+++ src/ttablemaker.py
@@ -1,14 +1,18 @@
-from parser import *
+from tmp import *
 from tabulate import tabulate
 
-scheduled = result_graph['xtsgndp'][True]
-
-ttable = [[ [] for i in range(p_max) ] for i in range(len(days))]
-
-for (t, s, g, n, d, p) in scheduled:
-    ttable[d][p].append((t, s, g, n))
-
-for i in range(len(ttable)):
-    ttable[i].insert(0, "Day {}".format(str(i)))
-
-print tabulate(ttable, headers=["X"]+range(p_max), tablefmt='fancy_grid').encode('utf-8')
+for sol in all_result:
+    result_graph = all_result[sol]
+    scheduled = result_graph['xtsgndp'][True]
+    
+    ttable = [[ [] for i in range(p_max) ] for i in range(len(days))]
+    
+    for (t, s, g, n, d, p) in scheduled:
+        if courses[(t, s)] not in ttable[d][p]:
+            ttable[d][p].append(courses[(t, s)])
+        
+    for i in range(len(ttable)):
+        ttable[i].insert(0, "Day {}".format(str(i)))
+    
+    print tabulate(ttable, headers=["X"]+range(p_max), tablefmt='fancy_grid').encode('utf-8')
+    print
