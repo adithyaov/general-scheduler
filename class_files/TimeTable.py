3550110d3c600c148a1d59b25cd5a7dce16310a0
Adithya Kumar <111501017@smail.iitpkd.ac.in>
2018-03-30 22:23:42 +0530

    Started making classes

---
+++ class_files/TimeTable.py
@@ -0,0 +1,10 @@
+class TimeTable():
+	"""docstring for TimeTable"""
+	var_result = {}
+	bool_list = []
+	truth_dict = {}
+	bool_graph = {}
+	def __init__(self, graph):
+		super(TimeTable, self).__init__()
+		self.graph = graph
+		
